dc5478e2406d01a0e493b033518d6685e1acacb2
Stephen Eilert <spedrosa@gmail.com>
2010-11-12 20:07:05 -0300

    - Methods 'first' and 'all' implemented and working. - It is possible to add "conditions" (if they are quoted)

--- evedatabase.scm
+++ evedatabase.scm
@@ -43,11 +43,11 @@
 (define-method (list-columns (model <model>))
   (slot-value (get-table-class model) 'columns))
 
-(define-method (all (model <model>))
-  (execute-sql (eval `(from ,(get-table-name model) (,@(list-columns model))))))
+(define (all class #!key conditions)
+  (load-data class (execute-sql (eval `(from ,(get-table-name (make class)) (,@(list-columns (make class))) ,conditions)))))
 
-(define-method (first (model <model>))
-  (execute-sql (eval `(from ,(get-table-name model) (,@(list-columns model)) (limit 1)))))
+(define (first class #!key conditions)
+  (car (load-data class (execute-sql (eval `(from ,(get-table-name (make class)) (,@(list-columns (make class))) ,conditions (limit 1)))))))
 
 (define-method (save (model <model>))
   #f)
@@ -57,6 +57,24 @@
 		      (lambda (database)
 			(query fetch-all (sql database stmt)))))
 
+(define (load-data class data)
+  (map (lambda (row)
+	 (let ([newobject (make class)])
+	   (for-each (lambda (column model-column)
+		       (set! (slot-value newobject model-column) column)) row (list-columns newobject))
+	   newobject)) data))
+
+(define-model <inventory-categories> "invcategories"
+  (categoryID categoryName description graphicID published))
+
+(define-model <inventory-type-reactions> "invtypereactions"
+  (reactionTypeID input typeID quantity))
+
+(define-model <inventory-groups> "invgroups"
+  (groupID categoryID groupName description graphicID useBasePrice allowManufacture allowRecycler anchored anchorable fittableNonSingleton published))
+
+(define-model <inventory-types> "invtypes"
+  (typeID groupID typeName description graphicID radius mass volume capacity portionSize raceID basePrice published marketGroupID chanceOfDuplicating))
 
 ;; "select * from invcategories where categoryID in (41,42,43);"
 ;; [
@@ -69,6 +87,10 @@
 "select * from invgroups as i join invcategories as j on i.categoryID = j.categoryID 
 						join invtypes as t on i.groupID = t.groupID
 									 and i.categoryID = 43"
+
+;; [
+;;     { "groupID" : """0""", "categoryID" : """0""", "groupName" : """#System""", "description" : """""", "graphicID" : """""", "useBasePrice" : """0""", "allowManufacture" : """1""", "allowRecycler" : """1""", "anchored" : """0""", "anchorable" : """0""", "fittableNonSingleton" : """0""", "published" : """0""" },
+;; ]
 
 ;; Only the most interesting rows
 "select typeID, groupName, typeName  from invgroups as i join invcategories as j on i.categoryID = j.categoryID 
