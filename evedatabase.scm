943f6d51fa441bb8340023912f4300be9b397120
Stephen Eilert <spedrosa@gmail.com>
2010-11-19 18:46:16 -0300

    - Figured out I was using the wrong tables.

--- evedatabase.scm
+++ evedatabase.scm
@@ -12,16 +12,54 @@
 (define-model <inventory-types> "invtypes"
   (typeID groupID typeName description graphicID radius mass volume capacity portionSize raceID basePrice published marketGroupID chanceOfDuplicating))
 
+(define-model <planet-schematics> "planetSchematics"
+  (schematicID schematicName cycleTime))
+
+(define-model <planet-schematics-typemap> "planetschematicstypemap"
+  (schematicID typeID quantity isInput))
+
 (define (get-planetary-category)
   (car (all <inventory-categories> conditions: '(where (= 'categoryID 43)))))
 
 (define (get-planetary-groups category-id)
-  (all <inventory-groups> conditions: '(where (= 'categoryID category-id))))
+  (all <inventory-groups> conditions: `(where (= 'categoryID ,category-id))))
 
 (define (get-planetary-items)
   (let ([category (get-planetary-category)])
     (map (lambda (group)
-	   (all <inventory-types> conditions: '(where (= 'groupID (slot-value group 'groupID))))) (get-planetary-groups (slot-value category 'categoryID)))))
+	   (all <inventory-types> conditions: `(where (= 'groupID ,(slot-value group 'groupID))))) (get-planetary-groups (slot-value category 'categoryID)))))
+
+;; Function below can be adapted to moon mining.
+;; (define (get-production-tree item)
+;;   (let ([reactions (all <inventory-type-reactions> conditions: `(where (= 'typeID ,(slot-value item 'typeID))))])
+;;     (if (null? reactions)
+;; 	item
+;; 	(map (lambda (reaction-item)
+;; 	       (get-production-tree reaction-item)) reactions))))
+
+;; (define-method (get-production-tree (schematic <planet-schematics>))
+;;   (let ([schematic-inputs (get-inputs schematic)])
+;;     (if (null? schematic-inputs)
+;; 	(get-output schematic))
+    
+
+(define-method (get-inputs (schematic <planet-schematics>))
+  (all <planet-schematics-typemap> conditions: `(where (and
+							(= 'schematicID ,(slot-value schematic 'schematicID))
+							(= 'isInput 1)))))
+
+(define-method (get-output (schematic <planet-schematics>))
+  (first <planet-schematics-typemap> conditions: `(where (and
+							(= 'schematicID ,(slot-value schematic 'schematicID))
+							(= 'isInput 0)))))
+
+(define-method (get-item (schematic <planet-schematics>))
+  (first <inventory-types> conditions: `(where (= 'typeID ,(slot-value (get-output schematic) 'typeID)))))
+
+(define-method (get-schematic (item <inventory-types>))
+  (first <planet-schematics-typemap> conditions: `(where (and
+							  (= 'typeID ,(slot-value item 'typeID))
+							  (= 'isInput 0)))))
 
 ;; "select * from invcategories where categoryID in (41,42,43);"
 ;; [
