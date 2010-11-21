2a406f77bc27b9ffbd7d3437534961093e2c20e9
Stephen <spedrosa@gmail.com>
2010-11-21 20:30:07 -0300

    - Added find-by-id method. - Some code cleaning up.

--- evedatabase.scm
+++ evedatabase.scm
@@ -1,16 +1,16 @@
 (include "orly")
 
 (define-model <inventory-categories> "invcategories"
-  (categoryID categoryName description graphicID published))
+  (categoryID categoryName description iconID published))
 
 (define-model <inventory-type-reactions> "invtypereactions"
   (reactionTypeID input typeID quantity))
 
 (define-model <inventory-groups> "invgroups"
-  (groupID categoryID groupName description graphicID useBasePrice allowManufacture allowRecycler anchored anchorable fittableNonSingleton published))
+  (groupID categoryID groupName description iconID useBasePrice allowManufacture allowRecycler anchored anchorable fittableNonSingleton published))
 
 (define-model <inventory-types> "invtypes"
-  (typeID groupID typeName description graphicID radius mass volume capacity portionSize raceID basePrice published marketGroupID chanceOfDuplicating))
+  (typeID groupID typeName description iconID radius mass volume capacity portionSize raceID basePrice published marketGroupID chanceOfDuplicating))
 
 (define-model <planet-schematics> "planetSchematics"
   (schematicID schematicName cycleTime))
@@ -22,27 +22,21 @@
   (car (all <inventory-categories> conditions: '(where (= 'categoryID 43)))))
 
 (define (get-planetary-groups category-id)
-  (all <inventory-groups> conditions: `(where (= 'categoryID ,category-id))))
+  (find-by-id <inventory-groups> category-id))
 
 (define (get-planetary-items)
   (let ([category (get-planetary-category)])
     (map (lambda (group)
 	   (all <inventory-types> conditions: `(where (= 'groupID ,(slot-value group 'groupID))))) (get-planetary-groups (slot-value category 'categoryID)))))
 
-;; Function below can be adapted to moon mining.
-;; (define (get-production-tree item)
-;;   (let ([reactions (all <inventory-type-reactions> conditions: `(where (= 'typeID ,(slot-value item 'typeID))))])
-;;     (if (null? reactions)
-;; 	item
-;; 	(map (lambda (reaction-item)
-;; 	       (get-production-tree reaction-item)) reactions))))
-
-;; (define-method (get-production-tree (schematic <planet-schematics>))
-;;   (let ([schematic-inputs (get-inputs schematic)])
-;;     (if (null? schematic-inputs)
-;; 	(get-output schematic))
-    
-
+(define (make-production-tree schematic #!optional acum)
+  (let ([inputs (get-inputs schematic)])
+    (if (null? inputs)
+        (append schematic acum)
+        (map
+         (lambda (input)
+               (make-production-tree (get-schematic (get-item input)) acum)) inputs))))
+         
 (define-method (get-inputs (schematic <planet-schematics>))
   (all <planet-schematics-typemap> conditions: `(where (and
 							(= 'schematicID ,(slot-value schematic 'schematicID))
@@ -54,38 +48,15 @@
 							(= 'isInput 0)))))
 
 (define-method (get-item (schematic <planet-schematics>))
-  (first <inventory-types> conditions: `(where (= 'typeID ,(slot-value (get-output schematic) 'typeID)))))
+  (first <inventory-types> conditions: `(where (= 'typeID ,(slot-value schematic 'typeID)))))
+
+(define-method (get-item (schematic-typemap <planet-schematics-typemap>))
+  (first <inventory-types> conditions: `(where (= 'typeID ,(slot-value schematic-typemap 'typeID)))))
 
 (define-method (get-schematic (item <inventory-types>))
-  (first <planet-schematics-typemap> conditions: `(where (and
+  (let ([schematic-typemap
+         (first <planet-schematics-typemap> conditions: `(where (and
 							  (= 'typeID ,(slot-value item 'typeID))
-							  (= 'isInput 0)))))
+							  (= 'isInput 0))))])
+    (find-by-id <planet-schematics> (slot-value schematic-typemap 'schematicID))))
 
-;; "select * from invcategories where categoryID in (41,42,43);"
-;; [
-;;     { "categoryID" : """41""", "categoryName" : """Planetary Interaction""", "description" : """Stuff for planetary interaction""", "graphicID" : """""", "published" : """0""" },
-;;     { "categoryID" : """42""", "categoryName" : """Planetary Resources""", "description" : """These are Items that can be extracted from a planet. """, "graphicID" : """""", "published" : """1""" },
-;;     { "categoryID" : """43""", "categoryName" : """Planetary Commodities""", "description" : """""", "graphicID" : """""", "published" : """1""" },
-;; ]
-
-;; Retrieve a list of planetary interaction items
-"select * from invgroups as i join invcategories as j on i.categoryID = j.categoryID 
-						join invtypes as t on i.groupID = t.groupID
-									 and i.categoryID = 43"
-
-;; [
-;;     { "groupID" : """0""", "categoryID" : """0""", "groupName" : """#System""", "description" : """""", "graphicID" : """""", "useBasePrice" : """0""", "allowManufacture" : """1""", "allowRecycler" : """1""", "anchored" : """0""", "anchorable" : """0""", "fittableNonSingleton" : """0""", "published" : """0""" },
-;; ]
-
-;; Only the most interesting rows
-"select typeID, groupName, typeName  from invgroups as i join invcategories as j on i.categoryID = j.categoryID 
-						join invtypes as t on i.groupID = t.groupID
-									 and i.categoryID = 43"
-
-;; Getting the typeIDs above allows us to check how they are reacted together
-"select * from invtypereactions limit 1"
-;; [
-;;     { "reactionTypeID" : """16868""", "input" : """0""", "typeID" : """3645""", "quantity" : """95""" },
-;; ]
-
-;; This will be enough for PI. For ship manufacturing, understanding of blueprints is required.
--- orly.scm
+++ orly.scm
@@ -5,7 +5,7 @@
 ;; (define-class model () (id))
 
 (define *database-url*
-  (make-parameter "eve.db"))
+  (make-parameter "tyr104-sqlite3-v1.db"))
 
 (define-class <table> ()
   (name columns (relationships initform: '())))
@@ -31,18 +31,38 @@
   (slot-value (get-table-class model) 'columns))
 
 (define (all class #!key conditions)
-  (let* ([klass (make class)]
-	 [table-name (get-table-name klass)]
-	 [table-columns (list-columns klass)])
-    (load-data class (execute-sql (eval (make-select table-columns table-name conditions: conditions))))))
+  (with-model class
+              (lambda (class table-name table-columns)
+                (load-data class (execute-sql (eval (make-select table-columns table-name conditions: conditions)))))))
 
 (define (first class #!key conditions)
-    (let* ([klass (make class)]
-	   [table-name (get-table-name klass)]
-	   [table-columns (list-columns klass)])
-      (car (load-data class (execute-sql (eval (append (make-select table-columns table-name conditions: conditions) '((limit 1)))))))))
+  (with-model class
+              (lambda (class table-name table-columns)
+                (car-if-not-empty (load-data class (execute-sql (eval (append (make-select table-columns table-name conditions: conditions) '((limit 1))))))))))
 
-(define (make-select columns table #!key conditions)
+(define (find-by-id class id)
+  (with-model class
+              (lambda (class table-name table-columns)
+                (car-if-not-empty (load-data class (execute-sql (eval (append (make-select table-columns table-name) `((where (= (quote ,(car table-columns)) ,id)))))))))))
+
+(define-syntax with-model
+  (syntax-rules ()
+    ([_ class function]
+     (let* ([klass (make class)]
+            [table-name (get-table-name klass)]
+            [table-columns (list-columns klass)])
+       (function class table-name table-columns)))))
+
+(define-syntax car-if-not-empty
+  (syntax-rules ()
+    ([_ expression]
+     (if (null? expression)
+         expression
+         (if (list? expression)
+             (car expression)
+             expression)))))
+
+(define (make-select columns table #!key conditions id)
   `(from ,table (,@columns) ,conditions))
 
 (define (make-where-id columns id)
@@ -58,11 +78,13 @@
 			(query fetch-all (sql database stmt)))))
 
 (define (load-data class data)
-  (map (lambda (row)
-	 (let ([newobject (make class)])
-	   (for-each (lambda (column model-column)
-		       (set! (slot-value newobject model-column) column)) row (list-columns newobject))
-	   newobject)) data))
+  (if (null? data)
+      #f
+      (map (lambda (row)
+             (let ([newobject (make class)])
+               (for-each (lambda (column model-column)
+                           (set! (slot-value newobject model-column) column)) row (list-columns newobject))
+               newobject)) data)))
 
 ;; (define-syntax has-many
 ;;   (syntax-rules (through:)
